0f657acaae19d1111b35c94159770b4cefd28332
mvmoshchovskyi <mvmoshchovskyi@gmail.com>
2020-07-09 21:52:22 +0300

    lesson 2

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lesson3';
+  constructor(private userService: UserService) {
+userService.getUsers().subscribe(value => console.log(value));
+  }
 }
---
+++ src/app/models/Comment.ts
@@ -0,0 +1,6 @@
+export interface Comment {
+  postId: number;
+  id: string;
+  title: string;
+  body: string;
+}
